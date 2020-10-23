9562e304e94e5838b5bc01ecd6e05de3bae38e36
Eric <eric_baffour-addo@alumni.brown.edu>
2020-10-22 23:38:57 -0700

    finished everything

--- source/index.js
+++ source/index.js
@@ -23,25 +23,113 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+const inventors1500=inventors.filter((inventor)=>{
+  return inventor.year>=1500&&inventor.year<=1600
+})
+console.log(inventors1500)
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
+const fullName=inventors.map((inventor)=>{
+  return `${inventor.first} ${inventor.last}`
+})
+console.log(fullName)
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
-
+inventors.sort((a,b)=>{
+  if(a.year<b.year){
+    return -1;
+  }else if(b.year<a.year){
+    return 1;
+  }else{
+    return 0
+  }
+})
+console.table(inventors)
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
-
+const yearsLived=inventors.reduce((total,inventor)=>{
+  return total+(inventor.passed-inventor.year)
+},0);
+console.log(yearsLived)
 // 5. Sort the inventors by years lived
+inventors.sort((a,b)=>{
+  if(a.passed-a.year<b.passed-b.year){
+    return 1;
+  }else if(b.passed-b.year<a.passed-a.year){
+    return -1
+  }else{
+    return 0
+  }
+})
+console.table(inventors)
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
+const links=Array.from(document.querySelectorAll(".mw-category-group li"));
+const cityNames=links.map((link)=>{
+  return link.textContent;
+})
 
+const de=cityNames.filter((city)=>{
+  return city.includes("de")
+})
+console.log(de)
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+console.table(people)
+const sortPeopleAlpha=()=>{
+  const alphabeticalArray= people.map((person)=>{
+     const reverseSplitArray=person.split(", ").reverse().join(", ");
+     return reverseSplitArray;
+  })
+  return alphabeticalArray.sort((a,b)=>{
+    const aItem=a.split(", ")[1];
+    const bItem=b.split(", ")[1]
+    if(aItem<bItem){
+      return -1;
+    }else if(bItem<aItem){
+      return 1;
+    }
+  })
+}
+console.log(sortPeopleAlpha());
+
+
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+/*
+const total=data.reduce((totalObj,item)=>{
+  if(item==="car"){
+    totalObj[item]++;
+  }else if(item==="truck"){
+    totalObj[item]++
+  }else if(item==="bike"){
+    totalObj[item]++
+  }else if(item==="van"){
+    totalObj[item]++
+  }
+  return totalObj;
+},{car:0,
+truck:0,
+bike:0,
+van:0,})
+*/
+
+
+const total=data.reduce((totalObject,item)=>{
+if(!totalObject[item]){
+  totalObject[item]=1;
+  console.log("hi")
+}else{
+  totalObject[item]++;
+  console.log("totalObject")
+}
+
+
+return totalObject;
+},{})
